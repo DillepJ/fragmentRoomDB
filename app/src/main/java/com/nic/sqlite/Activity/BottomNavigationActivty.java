a9fa4eb464989c1a4af7b55fc8cb0ba9a4f20c20
AchanthiSundar <tnrdmobile@gmail.com>
2022-01-05 15:33:29 +0530

    initial commit

--- app/src/main/java/com/nic/sqlite/Activity/BottomNavigationActivty.java
+++ app/src/main/java/com/nic/sqlite/Activity/BottomNavigationActivty.java
@@ -10,20 +10,21 @@ import meow.bottomnavigation.MeowBottomNavigation;
 
 public class BottomNavigationActivty extends AppCompatActivity {
 
-    MeowBottomNavigation bottomNavigation;
+    //MeowBottomNavigation bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation_activty);
 
-        bottomNavigation =findViewById(R.id.bottomNavigation);
+        /*bottomNavigation =findViewById(R.id.bottomNavigation);
 
         bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.ic_home));
         bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.ic_account));
         bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.ic_message));
         bottomNavigation.add(new MeowBottomNavigation.Model(4, R.drawable.ic_explore));
         bottomNavigation.add(new MeowBottomNavigation.Model(5, R.drawable.ic_notification));
+*/
 
     }
 }
